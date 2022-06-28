9af107aa0dbdeee2305a0bb64d315f0a7e31ea6f
Simon Knott <info@simonknott.de>
2022-06-28 13:39:33 +0200

    point to main

--- netlify/edge-functions/site.ts
+++ netlify/edge-functions/site.ts
@@ -1,4 +1,4 @@
-import type { Context } from "https://deploy-preview-45--edge-bootstrap.netlify.app/v1/index.ts";
+import type { Context } from "https://edge-bootstrap.netlify.app/v1/index.ts";
 
 export default (request: Request, context: Context) => {
   return context.json(context.site);
