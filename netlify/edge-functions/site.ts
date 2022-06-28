f3599d445186fb57136e637cae136163737a5eb2
Simon Knott <info@simonknott.de>
2022-06-28 11:51:13 +0200

    import directly from url

--- netlify/edge-functions/site.ts
+++ netlify/edge-functions/site.ts
@@ -1,4 +1,4 @@
-import type { Context } from "netlify:edge";
+import type { Context } from "https://deploy-preview-45--edge-bootstrap.netlify.app/bootstrap/index-combined.ts";
 
 export default (request: Request, context: Context) => {
   return context.json(context.site);
